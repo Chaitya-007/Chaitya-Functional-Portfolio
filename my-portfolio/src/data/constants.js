57eb1c0bd66424983df18a2c50abbc9041c1de74
Chaitanya <chaitanyaasole@gmail.com>
2024-11-06 00:49:39 +0530

    tried to optimize

--- my-portfolio/src/data/constants.js
+++ my-portfolio/src/data/constants.js
@@ -1,63 +1,54 @@
-import {
-  backend,
-  github,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  redux,
-  tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
-  nextjs,
-  angularjs,
-  bootstrap,
-  materialui,
-  flutter,
-  express,
-  graphql,
-  python,
-  flask,
-  django,
-  mysql,
-  postgresql,
-  firebase,
-  aws,
-  googlecloud,
-  jenkins,
-  nginx,
-  grafana,
-  kubernetes,
-  promethus,
-  java,
-  kotlin,
-  jetpackcompose,
-  xml,
-  androidstudio,
-  tensorflow,
-  keras,
-  jupyter,
-  googlecolab,
-  sklearn,
-  netlify,
-  vscode,
-  postman,
-  adobexd,
-  pccoe,
-  kv,
-  powerbi,
-  bookstore,
-  fuelpriceprediction,
-  policefeedback,
-} from "../assets";
+import reactjs from "/tech/reactjs.png";
+import redux from "/tech/redux.png";
+import nextjs from "/tech/nextjs.png";
+import angularjs from "/tech/angularjs.png";
+import html from "/tech/html.png";
+import css from "/tech/css.png";
+import javascript from "/tech/javascript.png";
+import bootstrap from "/tech/bootstrap.png";
+import materialui from "/tech/materialui.png";
+import flutter from "/tech/flutter.png";
+import nodejs from "/tech/nodejs.png";
+import express from "/tech/express.png";
+import graphql from "/tech/graphql.png";
+import python from "/tech/python.png";
+import flask from "/tech/flask.png";
+import django from "/tech/django.png";
+import mysql from "/tech/mysql.png";
+import postgresql from "/tech/postgresql.png";
+import mongodb from "/tech/mongodb.png";
+import firebase from "/tech/firebase.png";
+import aws from "/tech/aws.png";
+import googlecloud from "/tech/googlecloud.png";
+import docker from "/tech/docker.png";
+import jenkins from "/tech/jenkins.png";
+import nginx from "/tech/nginx.png";
+import grafana from "/tech/grafana.png";
+import kubernetes from "/tech/kubernetes.png";
+import promethus from "/tech/promethus.png";
+import java from "/tech/java.png";
+import kotlin from "/tech/kotlin.png";
+import jetpackcompose from "/tech/jetpackcompose.png";
+import xml from "/tech/xml.png";
+import androidstudio from "/tech/androidstudio.png";
+import tensorflow from "/tech/tensorflow.png";
+import keras from "/tech/keras.png";
+import jupyter from "/tech/jupyter.png";
+import googlecolab from "/tech/googlecolab.png";
+import sklearn from "/tech/sklearn.png";
+import git from "/tech/git.png";
+import github from "/tech/github.png";
+import netlify from "/tech/netlify.png";
+import vscode from "/tech/vscode.png";
+import postman from "/tech/postman.png";
+import adobexd from "/tech/adobexd.png";
+import figma from "/tech/figma.png";
+import kv from "/kv.png";
+import pccoe from "/pccoe.png";
+import policefeedback from "/projects/policefeedback.png";
+import fuelpriceprediction from "/projects/fuelpriceprediction.png";
+import bookstore from "/projects/bookstore.png";
+import powerbi from "/projects/powerbi.png";
 
 export const Bio = {
   name: "Chaitanya Asole",
